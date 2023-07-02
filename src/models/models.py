802c214cb6dc60fbc847fe6b0c078e3e87f6d862
Харицкий Данил Константинович <Khlive.ftf@gmail.com>
2023-07-02 15:39:52 +0500

    feat: добавлена поддержка docker

--- src/models/models.py
+++ src/models/models.py
@@ -4,6 +4,15 @@ from pydantic import BaseModel, Field
 class PredictRequest(BaseModel):
     code: str = Field(title='Изначальный код для анализа', min_length=1)
 
+    class Config:
+        schema_extra = {
+            "example": [
+                {
+                    "code": "public bool IsStringWithUniqueSymbols(",
+                }
+            ]
+        }
+
 
 class PredictResponse(BaseModel):
     result: str = Field(title='Результат предиктора', min_length=1)
