beec05f63d957702506c969b2331e98e19146d3d
ruslan.zagidullin <zagidullin.ray@gmail.com>
2023-07-05 23:46:11 +0500

    Some new engines

--- src/python/calculations/predictor_engine.py
+++ src/python/calculations/predictor_engine.py
@@ -1,4 +1,4 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModelForSeq2SeqLM
 
 
 class PredictorEngine:
@@ -8,13 +8,34 @@ class PredictorEngine:
         self.device = "cpu"
 
     def predict(self, text):
-        tokenizer = AutoTokenizer.from_pretrained(self.model)
+        tokenizer = AutoTokenizer.from_pretrained(self.model, load_in_4bit=True)
         model = AutoModelForCausalLM.from_pretrained(self.model, trust_remote_code=True, revision="main").to(self.device)
 
         input_ids = tokenizer(text, return_tensors="pt").input_ids
         generated_ids = model.generate(input_ids, max_length=128, pad_token_id=tokenizer.eos_token_id)
         result = text + tokenizer.decode(generated_ids[0], skip_special_tokens=False)[len(text):]
         return result
+
+    def predict2(self, text, pattern):
+        tokenizer = AutoTokenizer.from_pretrained(self.model, load_in_4bit=True)
+        model = AutoModelForCausalLM.from_pretrained(self.model, trust_remote_code=True, revision="main")
+        inputs = tokenizer(text, return_tensors="pt")
+        sample = model.generate(**inputs, max_length=128, pad_token_id=tokenizer.eos_token_id)
+        result = tokenizer.decode(sample[0], truncate_before_pattern=pattern)
+        return result
+
+    def answer(self, question, context):
+        tokenizer = AutoTokenizer.from_pretrained(self.model, load_in_4bit=True)
+        model = AutoModelForSeq2SeqLM.from_pretrained(self.model)
+        input = f"question: {question} context: {context}"
+        encoded_input = tokenizer([input],
+                                  return_tensors='pt',
+                                  truncation=True)
+        output = model.generate(input_ids=encoded_input.input_ids,
+                                attention_mask=encoded_input.attention_mask,
+                                max_new_tokens=128)
+        output = tokenizer.decode(output[0], skip_special_tokens=True)
+        return output
 
     @staticmethod
     def __get_model(model_type: str):
@@ -25,4 +46,6 @@ class PredictorEngine:
             'Codegen2_16': 'Salesforce/codegen2-16B',
             'Codet5p_2': 'Salesforce/codet5p-2b',
             'Starcoder': 'bigcode/starcoder',
+            'facebook_125': 'facebook/opt-125m',
+            't5_base': 'MaRiOrOsSi/t5-base-finetuned-question-answering',
         }.get(model_type)
--- src/python/tests/predict_engine_test.py
+++ src/python/tests/predict_engine_test.py
@@ -1,12 +1,35 @@
 import unittest
 
-from calculations.predictor_engine import PredictorEngine
+from src.python.calculations.predictor_engine import PredictorEngine
 
 
 class PredictorEngineTest(unittest.TestCase):
-    def test_something(self):
+    def test_default(self):
         engine = PredictorEngine()
         result = engine.predict("public bool IsStringWithUniqueSymbols(")
+        print(result)
+        self.assertIsNotNone(result)
+
+    def test_default2(self):
+        engine = PredictorEngine("Codegen2_1")
+        result = engine.predict2(
+            "# this function check string with unique symbols on c#",
+            [r"\n\n^#", "^'''", "\n\n\n"]
+        )
+        print(result)
+        self.assertIsNotNone(result)
+
+    def test_facebook_125(self):
+        engine = PredictorEngine('facebook_125')
+        result = engine.predict("Hello, i'am a programmer and ")
+        print(result)
+        self.assertIsNotNone(result)
+
+    def test_question_answering(self):
+        engine = PredictorEngine('t5_base')
+        result = engine.answer(
+            "What difference between horizontal and vertical oil wells",
+            "oil")
         print(result)
         self.assertIsNotNone(result)
 
